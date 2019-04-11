ac579ed7cc20139fa42e88ab5b7ea72e6d4dda55
Marlon Monzon <martianworm17@gmail.com>
2019-04-11 23:19:46 +0800

    Add RecyclerView Sample

--- app/src/main/java/com/example/androidone/MainActivity.java
+++ app/src/main/java/com/example/androidone/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -11,12 +13,65 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         setSupportActionBar(toolbar);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"001",
+                "002",
+                "003",
+                "004",
+                "005",
+                "006",
+                "007",
+                "008",
+                "009",
+                "010",
+                "011",
+                "012",
+                "013",
+                "014",
+                "015",
+                "016",
+                "017",
+                "018",
+                "019",
+                "020",
+                "021",
+                "022",
+                "023",
+                "024",
+                "025",
+                "025",
+                "027",
+                "028",
+                "029",
+                "030",
+                "031",
+                "032",
+                "033"};
+
+
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
